e21cd99b517923350bcf22cbefe87a8fee7d14e2
christianykyo <christiany_kikyo_28rpl@smktelkom-mlg.sch.id>
2021-02-09 20:51:13 +0700

    Commit2

---
+++ app/src/main/java/com/dycode/edu/intent/SplashScreen.java
@@ -0,0 +1,27 @@
+package com.dycode.edu.intent;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+    private final int loading = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent home = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(home);
+                finish();
+
+            }
+        }, loading);
+    }
+}
